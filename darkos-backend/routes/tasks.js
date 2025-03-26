75a453014ff58a149e5af082183fc92daaf56ac4
DarkOS • On-Chain Ops <darkosapp@gmail.com>
2025-03-27 04:20:35 +0800

    frontend update
    frontend update

--- darkos-backend/routes/tasks.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const tasks = require('../data/exampleTask.json');
-
-router.get('/', (req, res) => {
-  res.json(tasks);
-});
-
-module.exports = router;
--- darkos-backend/utils/aliasGenerator.js
+++
@@ -1,9 +0,0 @@
-const adjectives = ['silent', 'unit', 'ghost', 'dark', 'flux', 'echo'];
-const numbers = () => Math.floor(100 + Math.random() * 900);
-
-function generateAlias() {
-  const adj = adjectives[Math.floor(Math.random() * adjectives.length)];
-  return `${adj}${numbers()}.darkos`;
-}
-
-module.exports = generateAlias;
